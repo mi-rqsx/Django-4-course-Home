916944350a1360bc0f3bde55f07aae32176b1981
mi-rqsx <r.kim1945@gmail.com>
2024-01-19 18:02:22 +0600

    Full text search well donegit add .!

--- goods/utils.py
+++ goods/utils.py
@@ -1,16 +1,48 @@
 from django.db.models import Q
-from django.contrib.postgres.search import SearchVector, SearchRank, SearchQuery
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchRank,
+    SearchQuery,
+    SearchHeadline,
+)
 from goods.models import Products
 
 
 def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
-    
+
     vector = SearchVector("name", "description")
     query = SearchQuery(query)
 
-    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
+    results = (
+        Products.objects.annotate(rank=SearchRank(vector, query))
+        .filter(rank__gt=0)
+        .order_by("-rank")
+    )
+
+    results = results.annotate(
+        headline=SearchHeadline(
+            "name",
+            query,
+            start_sel="<span style='background-color: yellow'>", # see catalog.html line 68 - 74
+            stop_sel="</span>",
+        )
+    )
+
+    results = results.annotate(
+        bodyline=SearchHeadline(
+            "description",
+            query,
+            start_sel="<span style='background-color: yellow'>", # see catalog.html line 79 - 85
+            stop_sel="</span>",
+        )
+    )
+
+    return results
+
+
+    # return Products.objects.annotate(rank=SearchRank(vector, query)).filter(rank__gt=0).order_by("-rank")
 
     # keywords = [word for word in query.split() if len(word) > 2]
 
